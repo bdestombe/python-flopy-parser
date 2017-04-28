4aa5eafb74b60ef08b0ad96f8d7c5a47ba5bd8ec
Bas des Tombe <bdestombe@gmail.com>
2017-04-28 18:32:08 +0200

    Cleaned up the command line interface with argparse

--- flopymetascript/flopymetascript.py
+++ flopymetascript/flopymetascript.py
@@ -16,64 +16,58 @@ process can also be run, where all its arguments are file handles
 
 def main():
     """
-    The base64 input and output can either be a filename or a pipe.
-
-    # Example usage:
-
-    $ python metascript.py --outbytesfile output.zip --inbytesfile input.zip
-    input.zip is a zip-file that contains MODFLOW input files and a single .nam file. Its content is processed and 
-    written to output.zip.
-
-    $ openssl base64 -in input.zip -out input.zip.b64
-    $ python metascript.py --outbytesfile output.zip --inbase64file input.zip.b64
-    input.zip is encoded to base64 and is used as input file for metascript.py
-
-    $ python metascript.py --outbytesfile output.zip --inbase64file - < input.zip.b64
-    The content of input.zip.b64 is streamed/piped to metascript.py
-
-    $ openssl base64 -in input.zip | python metascript.py --outbytesfile output.zip --inbase64file -
-    The same as what is done previously, however input.zip is encoded and instead of writing it to a file, it is passed
-    as stdin to the inbase64file argument.
-
-    todo:
-    - include setuptools
-    - prepare tests. pymake check tools to compare two 
+    Provides the command line interface receives either filenames, stdin, or stdout, sanitizes them and calls the  
+    process function with file handles.
+    :return: 
     """
-
     usage = """
-    Example usage:
+## With zipfiles
+Try this first,
+```bash
+$ flopymetascript --outbytesfile output.zip --inbytesfile input.zip --logfile log.txt
+```
+input.zip is a zip-file that contains MODFLOW input files and a single .nam file. Its content is processed and 
+written to output.zip. Some logging is written to log.txt. The `$`-sign should be omitted, and only refers to that the 
+command is to be entered in the bash-commandline.
 
-    $ python metascript.py --outbytesfile output.zip --inbytesfile input.zip
-    input.zip is a zip-file that contains MODFLOW input files and a single .nam file. Its content is processed and 
-    written to output.zip.
+## Using pipes
+```bash
+$ openssl base64 -in input.zip -out input.zip.b64
+$ flopymetascript --outbytesfile output.zip --inbase64file input.zip.b64
+```
+input.zip is encoded to base64 and is used as input file for flopymetascript
 
-    $ openssl base64 -in input.zip -out input.zip.b64
-    $ python metascript.py --outbytesfile output.zip --inbase64file input.zip.b64
-    input.zip is encoded to base64 and is used as input file for metascript.py
+```bash
+$ flopymetascript --outbytesfile output.zip --inbase64file - < input.zip.b64
+```
+The content of input.zip.b64 is streamed/piped to flopymetascript
 
-    $ python metascript.py --outbytesfile output.zip --inbase64file - < input.zip.b64
-    The content of input.zip.b64 is streamed/piped to metascript.py
+```bash
+$ openssl base64 -in input.zip | flopymetascript --outbytesfile output.zip --inbase64file -
+```
+The same as what is done previously, however input.zip is encoded and instead of writing it to a file, it is passed
+as stdin to the inbase64file argument of flopymetascript.
 
-    $ openssl base64 -in input.zip | python metascript.py --outbytesfile output.zip --inbase64file -
-    The same as what is done previously, however input.zip is encoded and instead of writing it to a file, it is passed
-    as stdin to the inbase64file argument.
+```bash
+$ openssl base64 -in input.zip | flopymetascript --outbase64file utput.zip --inbase64file - --logfile -
+```
+The log file is printed to stdout.
 
-    todo:
-    - include setuptools
-    - prepare tests. pymake check tools to compare two 
-    """
+You cannot send both outbase64file and logfile to stdout. They will be mixed and the resulting output file is not 
+readable.
+"""
 
     description = """
-    Converts a zip with MODFLOW input files to a zip containing Flopy script
-    """
+Converts a zip with MODFLOW input files to a zip containing Flopy script
+"""
 
     epilog = """
-    No money is to be made with this service. I you find it useful, please donate to charity (be creative in choosing 
-    which one) and send me a note. Thanks! The author is not affiliated with the modflow family nor Flopy. This 
-    converter/generator uses the Flopy load function. Any errors/mistakes in the Flopy load functions propagate to the 
-    generated script. The author has absolutely no convidense in that this script is correct and is not responsible for 
-    the content and consequences of malicious scripts.
-    """
+No money is to be made with this service. I you find it useful, please donate to charity (be creative in choosing 
+which one) and send me a note. Thanks! The author is not affiliated with the modflow family nor Flopy. This 
+converter/generator uses the Flopy load function. Any errors/mistakes in the Flopy load functions propagate to the 
+generated script. The author has absolutely no convidense in that this script is correct and is not responsible for 
+the content and consequences of malicious scripts.
+"""
 
     class MyParser(argparse.ArgumentParser):
         def error(self, message):
@@ -81,20 +75,31 @@ def main():
             self.print_help()
             sys.exit(2)
 
-    parser = MyParser(prog='metascript',
+    class input_action(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            if values.name == "<stdin>" or values.name == "<stdout>":
+                raise argparse.ArgumentTypeError("Cannot use stdin for inbytesfile or stdout for outbytesfile")
+            setattr(namespace, self.dest, values)
+
+    parser = MyParser(prog='flopymetascript',
                       usage=usage,
                       description=description,
                       epilog=epilog)
-    parser.add_argument('--inbase64file', type=argparse.FileType('r'),
-                        required=False)
-    parser.add_argument('--outbase64file', type=argparse.FileType('w'),
-                        required=False)
-    parser.add_argument('--inbytesfile', type=argparse.FileType('rb'),
-                        required=False)
-    parser.add_argument('--outbytesfile', type=argparse.FileType('wb'),
-                        required=False)
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+
+    inputs = parser.add_mutually_exclusive_group()
+    output = parser.add_mutually_exclusive_group()
+
+    inputs.add_argument('--inbase64file', type=argparse.FileType('r'),
+                        required=False, help='Filename or - for stdin of the input zipfile')
+    output.add_argument('--outbase64file', type=argparse.FileType('w'),
+                        required=False, help='Filename or - for stdout of the output zipfile')
+    inputs.add_argument('--inbytesfile', type=argparse.FileType('rb'),
+                        required=False, action=input_action, help='Filename of the input zipfile')
+    output.add_argument('--outbytesfile', type=argparse.FileType('wb'),
+                        required=False, action=input_action, help='Filename of the output zipfile')
     parser.add_argument('--logfile', type=argparse.FileType('w'),
-                        required=False)
+                        required=False, help='Filename or - for stdout of the logfile')
 
     # Access the arguments as a dictionary
     kwargs = vars(parser.parse_args())
@@ -107,13 +112,13 @@ def process(inbase64file=None, outbase64file=None,
             inbytesfile=None, outbytesfile=None,
             logfile=None):
     """
-    bytefiles can not be used for piping. because printing to stdin and stdout assumes utf8.
+    All arguments are filehandles. Assumes sane filehandles, no checking for incompatible stdin stdout combinations.
 
-    :param logfile: 
-    :param inbase64file: File handle that has a read utf8 attribute that are encoded with base64
-    :param outbase64file: File handle that has a write utf8 attribute that are encoded with base64
-    :param inbytesfile: File handle that has a read bytes attribute
-    :param outbytesfile: File handle that has a write bytes attribute
+    :param logfile: File handle with a write utf8 attribute of the logfile
+    :param inbase64file: File handle with a read utf8 attribute of the input zipfile and encoded with base64
+    :param outbase64file: File handle with a write utf8 attribute of the output zipfile and is encoded with base64
+    :param inbytesfile: File handle with a read bytes attribute of the input zipfile
+    :param outbytesfile: File handle with a write bytes attribute of the output zipfile
     :return: 
     """
     stdout_buf = io.StringIO()
