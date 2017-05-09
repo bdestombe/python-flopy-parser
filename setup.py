81c116bc2531bcde56e482248856e3f2a49cfcc5
Bas des Tombe <bdestombe@gmail.com>
2017-05-09 08:13:09 +0200

    Added last pypi required files. PEP8 formatted using yapf.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import sys
 
 from setuptools import setup
 
+from flopymetascript import __version__, __name__, __author__
+
 if sys.version_info[0] != 3 or sys.version_info[1] < 4:
     print("""
         This script requires Python version 3.4.
@@ -13,17 +15,45 @@ if sys.version_info[0] != 3 or sys.version_info[1] < 4:
         """)
     sys.exit(1)
 
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r", "")
+
+except OSError as e:
+    import io
+
+    # pandoc is not installed, fallback to using raw contents
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
 setup(
-    name='flopymetascript',
+    name=__name__,
     description=
     'Converts a zip with MODFLOW input files to a zip containing Flopy script',
-    version='0.2.0',
+    long_description=long_description,
+    version=__version__,
     packages=['flopymetascript'],
     license='MIT',
-    author='Bas des Tombe',
+    author=__author__,
     author_email='bdestombe@gmail.com',
     url='https://github.com/bdestombe/flopymetascript',
-    install_requires=['numpy>=1.12', 'nbformat>=4.3', 'nbconvert>=5.1', 'flopy>=3.2', 'yapf'],
+    download_url=
+    'https://github.com/bdestombe/flopymetascript/archive/0.2.0.tar.gz',
+    keywords=['flopy', 'groundwater', 'hydrology'],
+    install_requires=[
+        'numpy>=1.12', 'nbformat>=4.3', 'nbconvert>=5.1', 'flopy>=3.2', 'yapf'
+    ],
+    platforms='Windows, Mac OS-X',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+    ],
     entry_points={
         "console_scripts":
             ['flopymetascript = flopymetascript.flopymetascript:main']
