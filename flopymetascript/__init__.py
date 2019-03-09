e189e41d0bc1cfe5f21cf6af499537591f5d21f1
Bas des Tombe <bdestombe@gmail.com>
2019-03-09 15:07:32 +0100

    fix git ignore

--- flopymetascript/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-from .version import __version__
-
-__name__ = 'flopymetascript'
-__author__ = 'Bas des Tombe'
--- flopymetascript/version.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-major = 1
-minor = 0
-micro = 0
-
-__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
